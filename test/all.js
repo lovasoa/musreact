5d6e9fba7c50f38e781ed63a3e4756ab0cbface9
Ophir LOJKINE <pere.jobs@gmail.com>
2014-12-14 13:01:51 +0100

    Improve error logging

--- test/all.js
+++ test/all.js
@@ -10,7 +10,12 @@ exports["test files"] = function(assert){
   var parse = parser.parse.bind(parser);
   var testData = JSON.parse(fs.readFileSync(__dirname+"/tests.json", "utf8"));
   testData.tests.forEach(function(test){
-     var elemClass = eval(parse(test.source));
+     var jssource = parse(test.source);
+     try {
+        var elemClass = eval(jssource);
+     } catch(e) {
+       assert.fail("Invalid js was generated:\n" + jssource);
+     }
      var element = React.createElement(elemClass, test.data);
      var actual = React.renderToStaticMarkup(element);
      assert.equal(actual, test.expected, test.name);
